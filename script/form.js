72ee8c20aaf2ef902f704b9d6cea439d0822fe88
neinaskff <neina.skff@gmail.com>
2024-03-04 16:34:21 +0100

    finished form validation

--- script/form.js
+++ script/form.js
@@ -1,21 +1,30 @@
 (function () {
     const Form = {
         // для удобсва создаем отдельные свойства для полей формы, в которых будут находится все нужные данные для наших полей
+        agreeElement: null,
+        processElement: null,
+
         fields: [
             {
                 name: 'name',
                 id: 'name',
                 element: null,
+                regex: /^[A-Z][a-z]+\s*$/,
+                valid: false,
             },
             {
                 name: 'lastName',
                 id: 'last-name',
                 element: null,
+                regex: /^[A-Z][a-z]+\s*$/,
+                valid: false,
             },
             {
                 name: 'email',
                 id: 'email',
                 element: null,
+                regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                valid: false,
             }
         ],
         init() {
@@ -23,13 +32,56 @@
             this.fields.forEach(item => {
                 item.element = document.getElementById(item.id);
                 item.element.onchange = function () {
-                    this.validateField()
+                    that.validateField.call(that, item, this)
                 }
-            })
+            });
+
+            this.agreeElement = document.getElementById('agree');
+            this.agreeElement.onchange = function () {
+                that.validateForm();
+            }
+
+            this.processElement = document.getElementById('process');
+            this.processElement.onclick = function () {
+                that.processForm()
+            }
+
         },
         validateField(field, element) {
+            if (!element.value || !element.value.match(field.regex)) {
+                element.parentNode.style.borderColor = 'red';
+                field.valid = false;
+            } else {
+                element.parentNode.removeAttribute('style');
+                field.valid = true;
+            }
 
+            this.validateForm();
+        },
+        validateForm() {
+            const validForm = this.fields.every(item => item.valid);
+            const isValid = this.agreeElement.checked && validForm;
+            if (isValid) {
+                this.processElement.removeAttribute('disabled');
+            } else {
+                this.processElement.setAttribute('disabled', 'disabled');
+
+            }
+            return isValid;
+        },
+
+        processForm() {
+            if (this.validateForm()) {
+
+                let paramString = '';
+                this.forEach(item => {
+                    paramString += item.name;
+                })
+
+                location.href = 'choice.html?'
+            }
         }
-    }
+    };
     Form.init()
 })();
+
