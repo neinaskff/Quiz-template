b29009482b2b0f77242c2be355609403b4420392
neinaskff <neina.skff@gmail.com>
2024-03-11 16:06:41 +0100

    no message

--- script/choice.js
+++ script/choice.js
@@ -22,26 +22,47 @@
             }
         },
         processQuizzes() {
-
+            const choiceOptionsElement = document.getElementById('choice-options');
             if (this.quizzes && this.quizzes.length > 0) {
                 // делаем цикл, чтобы пройтись по каждому пришедшему объекту из бэкэнда
                 this.quizzes.forEach(quiz => {
+                    const that = this;
                     const choiceOptionElement = document.createElement('div');
                     choiceOptionElement.className = 'choice-option';
+                    choiceOptionElement.setAttribute('data-id', quiz.id);
+                    choiceOptionElement.onclick = function () {
+                        that.chooseQuiz(this)
+                    }
 
                     const choiceOptionTextElement = document.createElement('div');
                     choiceOptionTextElement.className = 'choice-option-text';
+                    choiceOptionTextElement.innerText = quiz.name;
 
                     const choiceOptionArrowElement = document.createElement('div');
                     choiceOptionArrowElement.className = 'choice-option-arrow';
 
-                    const choiceOptionImageElement = document.createElement('div');
+                    const choiceOptionImageElement = document.createElement('img');
                     choiceOptionImageElement.setAttribute('src', 'images/symbole-fleche-droite-violet.png');
+                    choiceOptionImageElement.setAttribute('alt', 'arrow');
+
+                    choiceOptionArrowElement.appendChild(choiceOptionImageElement);
+                    choiceOptionElement.appendChild(choiceOptionTextElement);
+                    choiceOptionElement.appendChild(choiceOptionArrowElement);
+
+                    choiceOptionsElement.appendChild(choiceOptionElement);
                 })
             }
             console.log(this.quizzes);
 
+        },
+
+        chooseQuiz(element) {
+            const dataId = element.getAttribute('data-id');
+            if (dataId) {
+                location.href = 'test.html' + location.search + '&id' + dataId;
+            }
         }
+
     }
 
     choice.init();
---
+++ script/test.js
@@ -0,0 +1,9 @@
+(function () {
+    const test = {
+        init() {
+            checkUserData();
+        }
+    }
+
+    test.init();
+})();
