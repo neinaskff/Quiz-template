8aa854bb20d4f1cabdd8bf6fc8304845789b0a5f
neinaskff <neina.skff@gmail.com>
2024-03-12 16:49:31 +0100

    add logic in test

--- script/test.js
+++ script/test.js
@@ -2,8 +2,12 @@
     const test = {
         quiz: null,
         questionTitleElement: null,
+        nextButtonElement: null,
+        prevButtonElement: null,
+        passButtonElement: null,
         optionsElement: null,
         currentQuestionIndex: 1,
+        userResult: [],
         init() {
             checkUserData();
             const url = new URL(location.href);
@@ -31,15 +35,45 @@
         startQuiz() {
             this.questionTitleElement = document.getElementById('title');
             this.optionsElement = document.getElementById('options');
+
+            this.nextButtonElement = document.getElementById('next');
+            this.nextButtonElement.onclick = this.move.bind(this, 'next');
+
+            this.passButtonElement = document.getElementById('pass');
+            this.passButtonElement.onclick = this.move.bind(this, 'pass');
+
+            this.prevButtonElement = document.getElementById('prev');
+            this.prevButtonElement.onclick = this.move.bind(this, 'prev');
+            document.getElementById('pre-title').innerText = this.quiz.name;
+
+            this.prepareProgressBar();
             this.showQuestion();
         },
 
+        prepareProgressBar() {
+
+
+            for (let i = 0; i < this.quiz.questions.length; i++) {
+                const itemElement = document.createElement('div');
+                itemElement.className = 'test-progress-bar-item ' + (i === 0 ? 'active' : '');
+
+            }
+        },
+
+
         showQuestion() {
+
             const activeQuestion = this.quiz.questions[this.currentQuestionIndex - 1];
-            this.questionTitleElement.innerHTML = '<span>Question ' + this.currentQuestionIndex +
-                ':</span> ' + activeQuestion.question;
+
+            this.questionTitleElement.innerHTML = '<span>Question ' + this.currentQuestionIndex + ':</span> ' + activeQuestion.question;
 
             this.optionsElement.innerHTML = '';
+            const that = this;
+
+            const chosenOption = this.userResult.find(item => {
+                return item.questionId === activeQuestion.id;
+            })
+
             activeQuestion.answers.forEach(answer => {
                 const optionElement = document.createElement('div');
                 optionElement.className = 'test-question-option';
@@ -47,10 +81,20 @@
                 const inputId = 'answer-' + answer.id;
 
                 const inputElement = document.createElement('input');
+
+                inputElement.className = 'option-answer';
                 inputElement.setAttribute('id', inputId);
                 inputElement.setAttribute('type', 'radio');
                 inputElement.setAttribute('name', 'answer');
                 inputElement.setAttribute('value', answer.id);
+
+                if (chosenOption && chosenOption.chosenAnswerId === answer.id) {
+                    inputElement.setAttribute('checked', 'checked')
+                }
+
+                inputElement.onchange = function () {
+                    that.chooseAnswer();
+                }
 
                 const labelElement = document.createElement('label');
                 labelElement.setAttribute('for', inputId);
@@ -59,9 +103,66 @@
                 optionElement.appendChild(inputElement);
                 optionElement.appendChild(labelElement);
 
-                this.optionsElement.appendChild(optionElement)
-
+                this.optionsElement.appendChild(optionElement);
             });
+
+            if (chosenOption && chosenOption.chosenAnswerId) {
+                this.nextButtonElement.removeAttribute('disabled');
+            } else {
+                this.nextButtonElement.setAttribute('disabled', 'disabled');
+            }
+
+            if (this.currentQuestionIndex === this.quiz.questions.length) {
+                this.nextButtonElement.innerText = 'End';
+            } else {
+                this.nextButtonElement.innerText = 'Next';
+
+            }
+
+            if (this.currentQuestionIndex > 1) {
+                this.prevButtonElement.removeAttribute('disabled');
+            } else {
+                this.prevButtonElement.setAttribute('disabled', 'disabled');
+
+            }
+        },
+        chooseAnswer() {
+            this.nextButtonElement.removeAttribute('disabled');
+        },
+        move(action) {
+            const activeQuestion = this.quiz.questions[this.currentQuestionIndex - 1];
+
+            const chosenAnswer = Array.from(document.getElementsByClassName('option-answer')).find(element => {
+                return element.checked;
+            })
+
+            let chosenAnswerId = null;
+            if (chosenAnswer && chosenAnswer.value) {
+                chosenAnswerId = Number(chosenAnswer.value);
+            }
+
+            const existingResult = this.userResult.find(item => {
+                return item.questionId === activeQuestion.id;
+            })
+
+            if (existingResult) {
+                existingResult.chosenAnswerId = chosenAnswerId;
+            } else {
+                this.userResult.push({
+                    questionId: activeQuestion.id,
+                    chosenAnswerId: chosenAnswerId,
+                })
+            }
+
+
+
+            console.log(this.userResult);
+            if (action === 'next' || action === 'pass') {
+                this.currentQuestionIndex++;
+            } else {
+                this.currentQuestionIndex--;
+            }
+            this.showQuestion();
         }
     }
 
